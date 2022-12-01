efe851952f6469dc3f2eb3a6f502245dde2e8fbd
Rumeli Paul <rumelipaul@gmail.com>
2022-12-01 14:52:51 +0100

    Implemented functionality with player creation and player movements

--- js/main.js
+++ js/main.js
@@ -1,1 +1,58 @@
-console.log("js loaded");
+//console.log("js loaded");
+class Player {
+  constructor() {
+    this.width = 10;
+    this.height = 10;
+    this.positionX = 50-(this.width/2);
+    this.positionY = 0;
+    this.domElement = null;
+    
+    this.createDomElement();
+   
+    
+  }
+
+  createDomElement() {
+    // step1: create the element:
+    this.domElement = document.createElement("div");
+
+    // step2: add content or modify (ex. innerHTML...)
+    this.domElement.id = "player";
+    this.domElement.style.width = this.width +"vw";
+    this.domElement.style.height = this.height + "vh";
+    this.domElement.style.bottom = this.positionY + "vh";
+    this.domElement.style.left = this.positionX + "vw";
+
+    //step3: append to the dom: `parentElm.appendChild()`
+    const boardElm = document.getElementById("board");
+    boardElm.appendChild(this.domElement);
+  }
+  moveRight() {
+    this.positionX = this.positionX + 5;
+    //update css for the player dom element
+    this.domElement.style.left = this.positionX + "vw";
+
+    console.log("new position..." + this.positionX);
+  }
+  moveLeft() {
+    
+    this.positionX -= 5;
+     //update css for the player dom element
+     this.domElement.style.left = this.positionX + "vw";
+
+    console.log("new position..." + this.positionX);
+  }
+}
+/////////////////////////////////////////
+
+const player = new Player();
+console.log("horizontal position :", player.positionX);
+
+document.addEventListener("keydown", function (event) {
+  const key = event.key; // "ArrowRight", "ArrowLeft", "ArrowUp", or "ArrowDown"
+  if (event.key === "ArrowLeft") {
+    player.moveLeft();
+  } else if (event.key === "ArrowRight") {
+    player.moveRight();
+  }
+});
